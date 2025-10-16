e27ac809a54fe81ddbc2ec48ed535688a8363f6f
‘Fred Oyetayo <fredoyetayo@yahoo.co.uk>
2025-10-16 22:04:11 +0100

    style: enhance editor sidebar UI with modern design and improved typography

--- build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '721eb573eeafab78ff4e');
